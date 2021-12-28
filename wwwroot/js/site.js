140590c6e6d947d3cde8e8e67b96cb7ea956ee7c
David Beesley <David.Beesley@tpr.gov.uk>
2021-12-28 21:30:02 +0000

    done some extra work to webapp

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -10,41 +10,52 @@ function getPayCalID(){
 }
 
 function payCalculator() {
-  getPayCalID();
-  console.log(x);
   fetch(`https://localhost:7229/PayCalculator?ID=${x}`)
     .then(data => console.log(data.text().then(function (text) {
       document.getElementById("payCalculatorOutput").innerHTML = text;
     })));
 }
 
-function AddEmployee_perm() {
-  document.getElementById("fname").visible;
-  document.getElementById("lname").visible;
-  document.getElementById("salary").visible;
-  document.getElementById("bonus").visible;
+function addEmployee_getinfo() {
+  document.getElementById("fields").style.display = "block";
+  document.getElementById("addEmployee_btn").style.display = "none";
 }
 
-function AddEmployee_temp(){
-  document.getElementById("fname").visible;
-  document.getElementById("lname").visible;
-  document.getElementById("dayrate").visible;
-  document.getElementById("weeksworked").visible;
+function addEmployee_perm() {
+  document.getElementById("perm_fields").style.display = "block";
+  document.getElementById("temp_fields").style.display = "none";
 }
 
-function dropdown() {
-  document.getElementById("employmentTypeDropdown").classList.toggle("show");
+function addEmployee_temp() {
+  document.getElementById("temp_fields").style.display = "block";
+  document.getElementById("perm_fields").style.display = "none";
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+function submitEmployee() {
+  isperm = null;
+  var fname = null;
+  var lname = null;
+  var salary_or_dayrate = null;
+  var bonus_or_weeksworked = null;
+  var ele = document.getElementsByName('is_perm');     
+  for(i = 0; i < ele.length; i++) {
+    if(ele[i].checked) {
+      isperm = ele[i].value;
+      fname = document.getElementById("fname").value;
+      lname = document.getElementById("lname").value;
+      if (isperm){
+        salary_or_dayrate = document.getElementById("salary").value;
+        bonus_or_weeksworked = document.getElementById("bonus").value;
       }
+      if (!isperm) {
+        salary_or_dayrate = document.getElementById("dayrate").value;
+        bonus_or_weeksworked = document.getElementById("weeksworked").value;
+      }
+      console.log(isperm, fname, lname, salary_or_dayrate, bonus_or_weeksworked);
+      fetch(`https://localhost:7229/AddEmployee?isperm=${isperm}&fname=${fname}&lname=${lname}salary_or_dayrate=${salary_or_dayrate}&bonus_or_weeksworked=${bonus_or_weeksworked}`, {
+      method: 'PUT'})
+        .then(data => console.log(data()));
+      break;
     }
   }
 }
