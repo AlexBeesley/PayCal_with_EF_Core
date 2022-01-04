76b017e3409fb2b396ba31f43cbb7f2c7583b4e3
David Beesley <David.Beesley@tpr.gov.uk>
2022-01-04 10:26:12 +0000

    clean code commit

--- PayCal API/Program.cs
+++ PayCal API/Program.cs
@@ -3,11 +3,7 @@ using PayCal;
 var builder = WebApplication.CreateBuilder(args);
 string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen( c =>
 {
@@ -17,19 +13,18 @@ builder.Services.AddSwaggerGen( c =>
 builder.Services.AddSingleton<IRepository<PermEmployeeData>, PermEmployeeRepository>();
 builder.Services.AddSingleton<IRepository<TempEmployeeData>, TempEmployeeRepository>();
 builder.Services.AddSingleton<Calculator>();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: MyAllowSpecificOrigins,
         builder =>
         {
-            // Replace with Path on your machine to get working.
+            // Replace path with one relavent to your machine in order to get the front end wwwroot site working.
             builder.WithOrigins("file:///C:/Users/beesleyd/source/repos/PayCal%20API/wwwroot/index.html");
         });
 });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -38,13 +33,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
 app.UseCors(MyAllowSpecificOrigins);
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
