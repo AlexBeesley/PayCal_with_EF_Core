e2f673d6d66646b189d0cda91cad65ce2ef99f5f
David Beesley <David.Beesley@tpr.gov.uk>
2022-01-05 15:07:17 +0000

    Further development post Harry and Alex 1-on-1.

---
+++ PayCal API/Controllers/PermEmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PayCal;
+
+namespace PayCal_API.Controllers
+{
+    public class PermEmployeeController : Controller
+    {
+        private PermEmployeeRepository perm;
+        private Calculator cal;
+
+        public PermEmployeeController(IRepository<PermEmployeeData> Perm)
+        {
+            perm = (PermEmployeeRepository)Perm;
+            cal = new Calculator(perm, null);
+        }
+
+        [HttpGet("~/Permanent/All-Employees")]
+        public string GetAllPermEmployees()
+        {
+            return ($"{(string.Concat(perm.ReadAll()))}");
+        }
+
+        [HttpGet("~/Permanent/Employee")]
+        public string GetPermEmployee(int ID)
+        {
+            return (string.Concat(perm.Read(ID)));
+        }
+
+        [HttpGet("~/Permanent/Employee/ID")]
+        public int GetPermEmployeeID(int ID)
+        {
+            return (perm.Read(ID).EmployeeID);
+        }
+
+        [HttpGet("~/Permanent/Employee/Employment-Type")]
+        public string GetPermEmploymentType(int ID)
+        {
+            return ($"Employee with ID: {perm.Read(ID).EmployeeID} is Permanent");
+        }
+
+        [HttpGet("~/Permanent/Employee/Full-Name")]
+        public string GetPermEmployeeFullName(int ID)
+        {
+            return ($"{perm.Read(ID).FName} {perm.Read(ID).LName}");
+        }
+
+        [HttpGet("~/Permanent/Employee/Salary")]
+        public int? GetPermSalary(int ID)
+        {
+            return (perm.Read(ID).Salaryint);
+        }
+
+        [HttpGet("~/Permanent/Employee/Bonus")]
+        public int? GetPermBonus(int ID)
+        {
+            return (perm.Read(ID).Bonusint);
+        }
+
+        [HttpPut("~/Permanent/NewEmployee")]
+        public bool PutNewPermEmployee(string fname, string lname, int salary, int bonus)
+        {
+            try
+            {
+                perm.Create(fname, lname, salary, bonus);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [HttpPut("~/Permanent/Delete-Employee")]
+        public bool DeletePermEmployee(int ID)
+        {
+            perm.Delete(ID);
+            return true;
+        }
+
+        [HttpGet("~/Permanent/Employee/Gross-Income")]
+        public double GetPermGrossIncome(int ID)
+        {
+            return (cal.CalculateEmployeePay(ID).Item1);
+        }
+
+        [HttpGet("~/Permanent/Employee/Income-After-Tax")]
+        public double GetPermIncomeAfterTax(int ID)
+        {
+            return (cal.CalculateEmployeePay(ID).Item2);
+        }
+    }
+}
---
+++ PayCal API/Controllers/TempEmployeeController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PayCal;
+
+namespace PayCal_API.Controllers
+{
+    public class TempEmployeeController : Controller
+    {
+        private TempEmployeeRepository temp;
+        private Calculator cal;
+
+        public TempEmployeeController(IRepository<TempEmployeeData> Temp)
+        {
+            temp = (TempEmployeeRepository)Temp;
+            cal = new Calculator(null, temp);
+        }
+
+        [HttpGet("~/Temporary/All-Employees")]
+        public string GetAllTempEmployees()
+        {
+            return ($"{(string.Concat(temp.ReadAll()))}");
+        }
+
+        [HttpGet("~/Temporary/Employee")]
+        public string GetTempEmployee(int ID)
+        {
+            return (string.Concat(temp.Read(ID)));
+        }
+
+        [HttpGet("~/Temporary/Employee/ID")]
+        public int GetTempEmployeeID(int ID)
+        {
+            return (temp.Read(ID).EmployeeID);
+        }
+
+        [HttpGet("~/Temporary/Employee/Employment-Type")]
+        public string GetTempEmploymentType(int ID)
+        {
+            return ($"Employee with ID: {temp.Read(ID).EmployeeID} is Temporary");
+        }
+
+        [HttpGet("~/Temporary/Employee/Full-Name")]
+        public string GetTempEmployeeFullName(int ID)
+        {
+            return ($"{temp.Read(ID).FName} {temp.Read(ID).LName}");
+        }
+
+        [HttpGet("~/Temporary/Employee/Day-Rate")]
+        public int? GetTempDayRate(int ID)
+        {
+            return (temp.Read(ID).DayRateint);
+        }
+
+        [HttpGet("~/Temporary/Employee/WeeksWorked")]
+        public int? GetTempWeeksWorked(int ID)
+        {
+            return (temp.Read(ID).WeeksWorkedint);
+        }
+
+        [HttpPut("~/Temporary/NewEmployee")]
+        public bool PutNewTempEmployee(string fname, string lname, int dayrate, int weeksworked)
+        {
+            try
+            {
+                temp.Create(fname, lname, dayrate, weeksworked);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        [HttpPut("~/Temporary/Delete-Employee")]
+        public bool DeleteTempEmployee(int ID)
+        {
+            temp.Delete(ID);
+            return true;
+        }
+
+        [HttpGet("~/Temporary/Employee/Gross-Income")]
+        public double GetTempGrossIncome(int ID)
+        {
+            return (cal.CalculateEmployeePay(ID).Item1);
+        }
+
+        [HttpGet("~/Temporary/Employee/Income-After-Tax")]
+        public double GetTempIncomeAfterTax(int ID)
+        {
+            return (cal.CalculateEmployeePay(ID).Item2);
+        }
+    }
+}
