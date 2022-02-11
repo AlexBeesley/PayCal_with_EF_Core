45a57f4198bf5b178b0df689d98dd5eaf4c4c18d
David Beesley <David.Beesley@tpr.gov.uk>
2022-02-11 14:57:41 +0000

    Removed styles attribute from all HTML.

--- PayCal MVC/wwwroot/js/site.js
+++ PayCal MVC/wwwroot/js/site.js
@@ -1,52 +1,31 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function ShowMenu_GetEmployeeBtn() {
-    var x = document.getElementById("getbyID_form");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else {
+function HiddenToggle(x) {
+    if (x.style.display === "block") {
         x.style.display = "none";
     }
+    else {
+        x.style.display = "block";
+    }
+}
+
+function ShowMenu_GetEmployeeBtn() {
+    HiddenToggle(document.getElementById("getbyID_form"));
 }
 
 function ShowMenu_PayCalBtn() {
-    var x = document.getElementById("PayCal_form");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else {
-        x.style.display = "none";
-    }
+    HiddenToggle(document.getElementById("PayCal_form"));
 }
 
 function ShowMenu_AddEmployeeBtn() {
-    var x = document.getElementById("Create_form");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else {
-        x.style.display = "none";
-    }
+    HiddenToggle(document.getElementById("Create_form"));
 }
 
 function ShowMenu_EditEmployeeBtn() {
-    var x = document.getElementById("Edit_form");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else {
-        x.style.display = "none";
-    }
+    HiddenToggle(document.getElementById("Edit_form"));
 }
 
 function ShowMenu_DeleteEmployeeBtn() {
-    var x = document.getElementById("Delete_form");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else {
-        x.style.display = "none";
-    }
+    HiddenToggle(document.getElementById("Delete_form"));
 }
